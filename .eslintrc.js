ed8aac6c32d3c21fe7d806504085ab03752c73e5
Damon Miller <damon@bigteams.com>
2020-09-22 22:27:48 -0400

    style(code:clean): ran code:clean against codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  rules: {
+    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+
+    // Recommend not to leave any console.log in your code
+    // Use console.error, console.warn and console.info instead
+    // https://eslint.org/docs/rules/no-console
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info'],
+      },
+    ],
+  },
 };
